596131f55216c83c3784699b1992f74ea1999d43
argerd <argerd14@gmail.com>
2019-11-15 19:44:59 +0400

    - создан Splash Screen.

---
+++ app/src/main/java/ru/argerd/repo/SplashActivity.java
@@ -0,0 +1,28 @@
+package ru.argerd.repo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = "Lol";
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ProgressBar progressBar = findViewById(R.id.splash_progress);
+        progressBar.setVisibility(ProgressBar.VISIBLE);
+
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+    }
+}
