b47e4a2b1e64d650ad658962febe4f28aad6fb3d
argerd <argerd14@gmail.com>
2019-11-19 17:21:03 +0400

    - рефакторинг кода;

--- app/src/main/java/ru/argerd/repo/SplashActivity.java
+++ app/src/main/java/ru/argerd/repo/SplashActivity.java
@@ -5,16 +5,11 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
-    static {
-        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-    }
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
