298328d00a33061a9db90ef77400cc7c4f3c4d77
Andrei <argerd14@gmail.com>
2019-12-01 01:48:14 +0400

    - практически окончена верстка экрана новостей.

---
+++ app/src/main/java/ru/argerd/repo/newsFragment/NewsAdapter.java
@@ -0,0 +1,66 @@
+package ru.argerd.repo.newsFragment;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import ru.argerd.repo.R;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.Holder> {
+    private int count;
+    private Context context;
+    private int[] photos;
+    private String[] titles;
+    private String[] newsContents;
+
+    NewsAdapter(int count, Context context, int[] photos,
+                String[] titles, String[] newsContents) {
+        this.count = count;
+        this.context = context;
+        this.photos = photos;
+        this.titles = titles;
+        this.newsContents = newsContents;
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        return new Holder(inflater.inflate(R.layout.item_of_news, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        holder.bind(photos[position], titles[position], newsContents[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return count;
+    }
+
+    class Holder extends RecyclerView.ViewHolder {
+        private ImageView photo;
+        private TextView title;
+        private TextView newsContent;
+
+        Holder(@NonNull View itemView) {
+            super(itemView);
+            photo = itemView.findViewById(R.id.photo_of_news);
+            title = itemView.findViewById(R.id.title_news);
+            newsContent = itemView.findViewById(R.id.news_content);
+        }
+
+        void bind(int photo, String title, String newsContent) {
+            this.photo.setImageResource(photo);
+            this.title.setText(title);
+            this.newsContent.setText(newsContent);
+        }
+    }
+}
--- app/src/main/java/ru/argerd/repo/newsFragment/NewsFragment.java
+++ app/src/main/java/ru/argerd/repo/newsFragment/NewsFragment.java
@@ -8,16 +8,32 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import ru.argerd.repo.R;
 
 public class NewsFragment extends Fragment {
+    private RecyclerView recyclerView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_news, container, false);
+
+        recyclerView = view.findViewById(R.id.news_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        int[] photos = {R.drawable.news_1, R.drawable.news_2};
+        String[] titles = {"Спонсоры отремонтируют школу-интернат",
+                "Спонсоры отремонтируют школу-интернат"};
+        String[] newsContent = {"Дубовская школа-интернат для детей с ограниченными возможностями " +
+                "здоровья стала первой в области блаблбалбалабалабалаблабалабалабалба",
+                "Дубовская школа-интернат для детей с ограниченными возможностями " +
+                        "здоровья стала первой в области блаблбалбалабалабалаблабалабалабалба"};
+        recyclerView.setAdapter(new NewsAdapter(2, getContext(), photos, titles,
+                newsContent));
 
         return view;
     }
