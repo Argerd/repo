9ca5efcb10b7436578ab485b885e3e754c5fc5c1
argerd <argerd14@gmail.com>
2020-01-02 16:51:41 +0400

    - небольшое исправление.

--- app/src/main/java/ru/argerd/repo/parsing/AsyncTaskEventsParsing.kt
+++ app/src/main/java/ru/argerd/repo/parsing/AsyncTaskEventsParsing.kt
@@ -13,7 +13,6 @@ class AsyncTaskEventsParsing(
         private val callback: (MutableList<Event>) -> Unit
 ) : AsyncTask<Void, Void, MutableList<Event>>() {
     private var settings: List<String>? = null
-    private var settingsSize = 0
     private var validEvents: MutableList<Event> = mutableListOf()
     private lateinit var listEvents: List<Event>
     private val parser = Parser()
@@ -29,23 +28,20 @@ class AsyncTaskEventsParsing(
         }
 
         settings?.let { settings ->
-            if (settingsSize != settings.size || validEvents.isEmpty()) {
-                settingsSize = settings.size
-                if (settings.isEmpty()) {
-                    validEvents = listEvents.toMutableList()
-                } else {
-                    validEvents.clear()
-                    for (i in listEvents.indices) {
-                        listEvents[i].categories?.let { category ->
-                            var counter = 0
-                            for (j in category.indices) {
-                                if (settings.contains(category[j]?.name)) {
-                                    counter++
-                                }
-                                if (counter == settings.size) {
-                                    validEvents.add(listEvents[i])
-                                    counter = 0
-                                }
+            if (settings.isEmpty()) {
+                validEvents = listEvents.toMutableList()
+            } else {
+                validEvents.clear()
+                for (i in listEvents.indices) {
+                    listEvents[i].categories?.let { category ->
+                        var counter = 0
+                        for (j in category.indices) {
+                            if (settings.contains(category[j]?.name)) {
+                                counter++
+                            }
+                            if (counter == settings.size) {
+                                validEvents.add(listEvents[i])
+                                counter = 0
                             }
                         }
                     }
