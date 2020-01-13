3d7df67a2cba2a98fb4d28002520f87b2973c57c
argerd <argerd14@gmail.com>
2020-01-13 15:19:18 +0400

    - упражнения по RxJava2.

--- app/src/main/java/ru/argerd/repo/model/Category.kt
+++
@@ -1,8 +0,0 @@
-package ru.argerd.repo.model
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class Category(var id: Int?,
-                    var name: String?) : Parcelable
